3e714445c71f2ccf2bb9e6c82074a94c09c2eb30
Heshegto Dorzhiev <dorzhiev_000@mail.ru>
2026-01-17 20:32:59 +0300

    added equals/hashCode methods as an Override and tests to them

--- src/main/java/ru/mentee/power/crm/domain/Lead.java
+++ src/main/java/ru/mentee/power/crm/domain/Lead.java
@@ -1,5 +1,7 @@
 package ru.mentee.power.crm.domain;
 
+import java.util.Objects;
+
 public class Lead {
   private String id;
   private String email;
@@ -40,5 +42,18 @@ public class Lead {
     return "Lead{id='" + id + "', email='" + email +
             "', phone='" + phone + "', company='" + company +
             "', status='" + status + "'}";
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this==o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    Lead lead = (Lead) o;
+    return Objects.equals(id, lead.id);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id);
   }
 }
