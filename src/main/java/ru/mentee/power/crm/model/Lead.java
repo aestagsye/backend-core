e12d0171c2289c8e2acdcfd0227e33dccc6db2e0
Heshegto Dorzhiev <dorzhiev_000@mail.ru>
2026-01-30 03:04:13 +0800

    Added classes LeadService, InMemoryLeadRepository, enum LeadStatus and updated record Lead, changed LeadRepository from class to interface to suit BCORE-9's requirements, deleted "useless" (in my opinion) class (and test to it), added test for class LeadService

--- src/main/java/ru/mentee/power/crm/model/Lead.java
+++ src/main/java/ru/mentee/power/crm/model/Lead.java
@@ -1,11 +1,12 @@
 package ru.mentee.power.crm.model;
 
+import java.util.UUID;
+
 public record Lead(
-        String id,
+        UUID id,
         String email,
-        String phone,
         String company,
-        String status
+        LeadStatus status
 ) {
   // Record автоматически генерирует equals/hashCode по всем полям
   // HashMap сможет использовать id как ключ
