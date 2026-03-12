fb1efa30249c387b5289596f291dd4ef85b75314
Heshegto Dorzhiev <dorzhiev_000@mail.ru>
2026-03-12 15:45:44 +0300

    BCORE-24: Added Validation -build.gradle: added implementations -form.jte: created form to show errors -Lead.java: added validation annotations -LeadController.java: changed 2 methods to add validation

--- src/main/java/ru/mentee/power/crm/model/Lead.java
+++ src/main/java/ru/mentee/power/crm/model/Lead.java
@@ -2,10 +2,21 @@ package ru.mentee.power.crm.model;
 
 import java.util.UUID;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
 public record Lead(
         UUID id,
+        @NotBlank(message = "Email обязателен")
+        @Email(message = "Некорректный формат email")
+        @Pattern(regexp = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$",
+                message = "Некорректный формат email")
         String email,
+        @NotBlank(message = "Company обязателен")
         String company,
+        @NotNull(message = "Статус обязателен")
         LeadStatus status
 ) {
 }
