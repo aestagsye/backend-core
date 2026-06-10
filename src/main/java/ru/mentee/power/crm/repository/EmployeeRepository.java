26f90e2b0e1d53b5870982e0b9be941197517ab9
Heshegto Dorzhiev <dorzhiev_000@mail.ru>
2026-06-10 12:56:38 +0300

    SpotlessApply

--- src/main/java/ru/mentee/power/crm/repository/EmployeeRepository.java
+++ src/main/java/ru/mentee/power/crm/repository/EmployeeRepository.java
@@ -4,5 +4,4 @@ import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.mentee.power.crm.domain.Employee;
 
-public interface EmployeeRepository extends JpaRepository<Employee, UUID> {
-}
+public interface EmployeeRepository extends JpaRepository<Employee, UUID> {}
